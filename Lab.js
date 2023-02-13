c835c2582f4e31c82ce57dfa3f685f8d123956e9
koryr5995 <122232214+koryr5995@users.noreply.github.com>
2023-02-13 14:32:31 -0500

    Add files via upload

---
+++ Lab.js
@@ -0,0 +1,12 @@
+let letterGrade = 70
+if (letterGrade >= 90){
+  console.log("A")
+} else if (letterGrade >= 80){
+  console.log("B")
+} else if (letterGrade >= 70){
+  console.log("C")
+} else if (letterGrade >= 55){
+  console.log("D")
+} else if (letterGrade < 55){
+  console.log("F")
+}
